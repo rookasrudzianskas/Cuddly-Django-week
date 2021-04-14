dd137602cd5050330edbe01abf45e37acca77960
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 12:13:22 +0300

    Rokas

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -3,9 +3,10 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 
+
 # Create your views here.
 
-def home(request, year, month):
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
     name = "Rokas"
     month = month.capitalize()
     # Convert month name to number
@@ -19,7 +20,7 @@ def home(request, year, month):
         year,
         month_number,
     )
-    #get current year
+    # get current year
 
     now = datetime.now()
     current_year = now.year
@@ -28,11 +29,11 @@ def home(request, year, month):
 
     time = now.strftime('%I:%M %p')
 
-    return render(request, 'home.html', {
+    return render(request, 'events/home.html', {
         "name": name,
         "year": year,
         "month": month,
-        "month_number":month_number,
+        "month_number": month_number,
         "cal": cal,
         "current_year": current_year,
         "time": time,
