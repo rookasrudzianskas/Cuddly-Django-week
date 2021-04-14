30752bfeef6ba1f84ec627bab4d60863288ecbee
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 12:42:37 +0300

    Rokas

--- myclub_website/events/admin.py
+++ myclub_website/events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Venue
+from .models import MyClubUser
+from .models import Event
 
-# Register your models here.
+
+admin.site.register(Venue)
+admin.site.register(MyClubUser)
+admin.site.register(Event)
+
+
--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Venue(models.Model):
+    name = models.CharField('Venue name', max_length=120)
+    address = models.CharField(max_length=300)
+    zip_code = models.CharField("Zip Code", max_length=15)
+    phone = models.CharField('Contact Phone', max_length=25)
+    web = models.URLField('Website Address')
+    email_address = models.EmailField('Email Address')
+
+    def __str__(self):
+        return self.name
+
+
+class MyClubUser(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField('User Email Adress')
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+
+class Event(models.Model):
+    name = models.CharField('Event Name', max_length=120)
+    event_date = models.DateTimeField('Event Date')
+    # venue = models.CharField(max_length=120)
+    venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
+    manager = models.CharField(max_length=60)
+    description = models.TextField(blank=True)
+    attendees = models.ManyToManyField(MyClubUser, blank=True)
+
+    def __str__(self):
+        return self.name
