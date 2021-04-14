1228737d40af93d5ab1490808988ef415ebec556
Rokas <rookas.rudzenskas@gmail.com>
2021-04-14 11:29:54 +0300

    Rokas

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    name = "Rokas"
+    return render(request, 'home.html', {
+        "name": name,
+    })
