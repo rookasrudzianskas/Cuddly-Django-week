4cff920d1a22d87a0e9e74d6d7ca41fb5155c47e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 10:56:41 +0300

    Rokas

--- myclub_website/events/admin.py
+++ myclub_website/events/admin.py
@@ -3,9 +3,21 @@ from .models import Venue
 from .models import MyClubUser
 from .models import Event
 
-
-admin.site.register(Venue)
+# admin.site.register(Venue)
 admin.site.register(MyClubUser)
-admin.site.register(Event)
+# admin.site.register(Event)
 
 
+@admin.register(Venue)
+class VenueAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'phone')
+    ordering = ('name',)
+    search_fields = ('name', 'address')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name', 'venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')
+    list_filter = ('event_date', 'venue')
+    ordering = ('-event_date',)
--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 
 class Venue(models.Model):
     name = models.CharField('Venue name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code = models.CharField("Zip Code", max_length=15)
-    phone = models.CharField('Contact Phone', max_length=25)
-    web = models.URLField('Website Address')
-    email_address = models.EmailField('Email Address')
+    phone = models.CharField('Contact Phone', max_length=25, blank=True)
+    web = models.URLField('Website Address', blank=True)
+    email_address = models.EmailField('Email Address',  blank=True)
 
     def __str__(self):
         return self.name
@@ -18,7 +20,7 @@ class Venue(models.Model):
 class MyClubUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    email = models.EmailField('User Email Adress')
+    email = models.EmailField('User Email Address')
 
     def __str__(self):
         return self.first_name + " " + self.last_name
@@ -29,7 +31,7 @@ class Event(models.Model):
     event_date = models.DateTimeField('Event Date')
     # venue = models.CharField(max_length=120)
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager = models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees = models.ManyToManyField(MyClubUser, blank=True)
 
