1847f69cffcf9b9fcb5ea8bb7da23a6a68481a79
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 10:31:28 +0300

    ROkas

--- myclub_website/events/forms.py
+++ myclub_website/events/forms.py
@@ -7,3 +7,20 @@ class VenueForm(ModelForm):
     class Meta:
         model = Venue
         fields = ('name', 'address', 'zip_code', 'phone', 'web', 'email_address')
+        labels = {
+            'name': '',
+            'address': '',
+            'zip_code': '',
+            'phone': '',
+            'web': '',
+            'email_address': '',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Venue Name'}),
+            'address': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Address'}),
+            'zip_code': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Zip Code'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone'}),
+            'web': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Web Address'}),
+            'email_address': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
+        }
