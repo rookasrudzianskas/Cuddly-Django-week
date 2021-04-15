1a3cb623f0f696999a2bbea3e58830422fd08f3e
Rokas <rookas.rudzenskas@gmail.com>
2021-04-15 10:25:03 +0300

    Rokas

--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -4,12 +4,27 @@ from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event
 from .forms import VenueForm
+from django.http import HttpResponseRedirect
 
 
 # Create your views here.
 
 def add_venue(request):
-    return render(request, 'events/add_venue.html', {})
+    submitted = False
+    if request.method == "POST":
+        form = VenueForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/add_venue?submitted=True')
+    else:
+        form = VenueForm
+        if 'submitted' in request.GET:
+            submitted = True
+
+    return render(request, 'events/add_venue.html', {
+        'form': form,
+        'submitted': submitted,
+    })
 
 
 def all_events(request):
