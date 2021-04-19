95a2c3a1b8ab85d819ffec58700b5c0d29eabd34
Rokas <rookas.rudzenskas@gmail.com>
2021-04-19 09:58:26 +0300

    Rokas

--- myclub_website/events/urls.py
+++ myclub_website/events/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('list_venues/', views.list_venues, name="list_venues"),
     path('show_venue/<venue_id>', views.show_venue, name="show-venue"),
     path('search_venues', views.search_venue, name="search-venues"),
+    path('update_venue/<venue_id>', views.update_venue, name="update-venue"),
 ]
--- myclub_website/events/views.py
+++ myclub_website/events/views.py
@@ -9,6 +9,13 @@ from django.http import HttpResponseRedirect
 
 # Create your views here.
 
+def update_venue(request, venue_id):
+    venue = Venue.objects.get(pk=venue_id)
+    return render(request, 'events/update_venue.html', {
+        'venue': venue,
+    })
+
+
 def search_venue(request):
     if request.method == "POST":
         searched = request.POST['searched']
