40d3b3682a6b2bc18d6a5aebbe1b1c8fef1fa250
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 15:10:47 +0300

    Rokas

--- myclub_website/members/urls.py
+++ myclub_website/members/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-
+    path('login_user', views.login_user, name="login")
 ]
 
--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -5,4 +5,12 @@ from django.contrib import messages
 
 # Create your views here.
 def login_user(request):
-    return render(request, 'authenticate/login.html', {})
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+
+    else:
+        return render(request, 'authenticate/login.html', {})
