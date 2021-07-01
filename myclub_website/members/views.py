9d32550aa8b496d567a1f74e4e58cae4b2217de8
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 15:00:11 +0300

    Rokas

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
+
 # Create your views here.
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})
