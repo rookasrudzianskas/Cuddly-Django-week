fa2ae51f0e6047ff5cf558f1a741d011342555a4
Rokas <rookas.rudzenskas@gmail.com>
2021-07-01 14:59:07 +0300

    Rokas

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
-
-
