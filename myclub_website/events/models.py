43d99acff1359ce0a082d8c2e3d8f0cbe863ee30
Rokas <rookas.rudzenskas@gmail.com>
2021-09-15 19:25:57 +0300

    Rokas

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -10,7 +10,7 @@ class Venue(models.Model):
     phone = models.CharField('Contact Phone', max_length=25, blank=True)
     web = models.URLField('Website Address', blank=True)
     email_address = models.EmailField('Email Address', blank=True)
-    owner = models.IntegerField('Owner', blank=True, null=True)
+    owner = models.IntegerField('Venue Owner', blank=False, null=True)
 
     def __str__(self):
         return self.name
