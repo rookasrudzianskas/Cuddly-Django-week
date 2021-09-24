e9b6bb0250fc8dedcb181f80f30921cb3d80deae
Rokas <rookas.rudzenskas@gmail.com>
2021-09-24 16:23:39 +0300

    Rokas

--- myclub_website/events/forms.py
+++ myclub_website/events/forms.py
@@ -26,6 +26,28 @@ class EventForm(ModelForm):
         }
 
 
+# user event form
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ('name', 'event_date', 'venue', 'attendees', 'description')
+        labels = {
+            'name': '',
+            'event_date': 'YYYY-MM-DD HH:MM:SS',
+            'venue': 'Venue',
+            'attendees': 'Attendees',
+            'description': '',
+        }
+
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Name'}),
+            'event_date': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Event Date'}),
+            'venue': forms.Select(attrs={'class': 'form-select', 'placeholder': 'Venue'}),
+            'attendees': forms.SelectMultiple(attrs={'class': 'form-select', 'placeholder': 'Attendees'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+        }
+
+
 class VenueForm(ModelForm):
     class Meta:
         model = Venue
