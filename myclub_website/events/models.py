e028b0df29535eca670e08c367a4017d8a48b6ab
Rokas <rookas.rudzenskas@gmail.com>
2021-11-21 12:55:19 +0200

    Roaks

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -16,7 +16,6 @@ class Venue(models.Model):
         return self.name
 
 
-
 class MyClubUser(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
